38ebf78f7fbbb4708dd972c9a7d496e84de26f5c
jailucloz@alum.us.es <jailucloz@alum.us,es>
2020-01-04 15:29:53 +0100

    configuraciones

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import django_heroku
+
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -113,7 +113,6 @@ DATABASES = {
     }
 }
 
-APIS = {}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
@@ -164,4 +163,4 @@ except ImportError:
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
 
-django_heroku.settings(locals())
+
